b6a24035dbfe757fa456cec9037120d5c3fb12b8
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-09 23:47:12 +0900

    create and style search bar for header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+    <header
+      className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5
+    md:px-10 items-center"
+    >
       {/* left */}
-      <div className="relative flex items-center h-32 cursor-pointer">
+      <div className="relative flex items-center h-32 cursor-pointer my-auto">
         <Image
           src="https://pbs.twimg.com/media/FInd8i-XwAIv8kp?format=jpg"
           layout="fill"
@@ -12,8 +16,22 @@ const Header = () => {
           objectPosition="left"
         />
       </div>
-      {/* Middle */}
-      <div></div>
+      {/* Middle - Search */}
+      <div
+        className="flex items-center md:border-2 rounded-full py-2 h-12
+      md:shadow-sm"
+      >
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm 
+          text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon
+          className="h-8 bg-red-400 text-white rounded-full p-1 cursor-pointer
+        hidden md:inline-flex md:mx-2"
+        />
+      </div>
       {/* Right */}
       <div></div>
     </header>
