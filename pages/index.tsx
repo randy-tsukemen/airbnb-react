bf231f0da0a5319531c37c2edd1ceee2b0b5aad7
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-10 13:57:52 +0900

    ssr loading data for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
-export default function Home() {
+export default function Home( { exploreData }) {
+  console.log(exploreData)
   return (
     <div className="">
       <Head>
@@ -13,6 +14,29 @@ export default function Home() {
       <Header />
       {/* Banner */}
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          {/* Pull some data from a server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      // Anything returned here will be available
+      // to the page via `props.whateverYouWantToName`
+      exploreData,
+    },
+  }
 }
