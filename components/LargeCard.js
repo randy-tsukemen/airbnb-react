0d3527ecc3e52a3140492f52a5fb3dd8b9251269
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-10 23:36:13 +0900

    adjust button style

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const LargeCard = ({ img, title, description, buttonText }) => {
   return (
-    <section className="relative py-16 cursor-pointer">
+    <section className="relative py-16">
       <div className="relative h-96 min-w-[300px]">
         <Image
           src={img}
@@ -14,7 +14,12 @@ const LargeCard = ({ img, title, description, buttonText }) => {
       <div className="absolute top-16 left-12">
         <h3 className="text-4xl mb-3 w-128">{title}</h3>
         <p>{description}</p>
-        <button className="text-white bg-cyan-900 px-4 py-2 rounded-lg mt-5 text-xl">
+        <button
+          className="text-white bg-cyan-900 px-4 py-2 
+        cursor-pointer rounded-lg mt-5 text-xl
+        hover:scale-105 transform transition duration-300
+        ease-out"
+        >
           {buttonText}
         </button>
       </div>
