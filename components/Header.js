78cd971398a564bc9ffa586afb7152935dcebb66
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-11 23:15:57 +0900

    Add no of guest input

--- components/Header.js
+++ components/Header.js
@@ -15,6 +15,7 @@ const Header = () => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
@@ -77,6 +78,21 @@ const Header = () => {
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl pl-2 flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-6" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg
+            outline-none text-red-400"
+            />
+          </div>
         </div>
       )}
     </header>
