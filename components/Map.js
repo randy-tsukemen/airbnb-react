7c67374a2e4c4bb4531cf78cd93b70d867d52021
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2022-01-14 22:21:49 +0900

    show pin on the mapbox

--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,7 @@
+import { getCenter } from "geolib";
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 const Map = ({ searchResults }) => {
-  const [viewport, setViewport] = useState({
-    width: "100%",
-    height: "100%",
-    latitude: 35.660833,
-    longitude: 139.766944,
-    zoom: 14,
-  });
-
   // Transform the searchResults into { latitude: 52.516272, longitude: 13.377722} object
   const coordinates = searchResults.map((result) => ({
     latitude: result.lat,
@@ -16,13 +9,37 @@ const Map = ({ searchResults }) => {
   }));
   console.log(coordinates);
 
+  const center = getCenter(coordinates);
+  console.log(center);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
   return (
     <ReactMapGL
       mapStyle={process.env.NEXT_PUBLIC_mapStyle}
       mapboxApiAccessToken={process.env.NEXT_PUBLIC_mapboxApiAccessToken}
       {...viewport}
       onViewportChange={(viewport) => setViewport(viewport)}
-    ></ReactMapGL>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p className="cursor-pointer text-2xl animate-bounce">🎃</p>
+          </Marker>
+        </div>
+      ))}
+    </ReactMapGL>
   );
 };
 
